adbadbc7d9497115f414b30dc284261a839d381d
Mizanur Rahman <private.mizan@gmail.com>
2026-03-31 09:51:26 +0600

    deploy

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
+import { Menu } from './pages/Menu';
+import { Gallery } from './pages/Gallery';
+import { Booking } from './pages/Booking';
+import { Review } from './pages/Review';
+import { FAQ } from './pages/FAQ';
+import { Contact } from './pages/Contact';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+export default function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="min-h-screen bg-black selection:bg-amber-500 selection:text-white">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/review" element={<Review />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
